8f7b8bf098d5489052738e0b6e67d7908ff2051b
mehraj <mehraj.shaikh24@gmail.com>
2021-09-02 01:47:51 +0530

    "Hello world only"

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Hello World");
+    printf("\n Enter a number : ");
+    scanf("%d", &num);
+    printf("\n Square of number is : %d",num*num);
+    return 0;
+}
